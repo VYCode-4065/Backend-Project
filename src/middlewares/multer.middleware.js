9f2b54d433015a7ab9fb2ba5810253bcaa6f3ad4
Vishal Yadav <yadavvishal4065@gmail.com>
2024-10-24 23:57:05 +0530

    Adding authorization of user

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,6 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     filename: function (req, file, cb) {
+        this.destination = "Public/temp";
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
         cb(null, file.fieldname + '-' + uniqueSuffix)
     }
